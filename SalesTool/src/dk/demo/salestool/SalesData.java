6a88c87384de83eaff7091760975432273a5dcc2
root <root@localhost>
2016-04-25 15:25:16 +0200

    Improvement for condition

--- SalesTool/src/dk/demo/salestool/SalesData.java
+++ SalesTool/src/dk/demo/salestool/SalesData.java
@@ -6,8 +6,8 @@ public class SalesData {
 
 	public void display() {
 		System.out.println("Data: ");
-		for (int i = 0; i < data.length; i++) {
-			System.out.println("Next value: " + data[i]);
+		for (int i:data) {
+			System.out.println("Next value: " + i);
 		}
 	}
 
