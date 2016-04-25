b550d7fb94c7bf400096fc6fbca73ea5cfcdec15
root <root@localhost>
2016-04-25 13:55:17 +0200

    Added a second test output.

--- SalesTool/src/dk/demo/salestool/Main.java
+++ SalesTool/src/dk/demo/salestool/Main.java
@@ -3,17 +3,18 @@ package dk.demo.salestool;
 public class Main {
 
 	public static void main(String[] args) {
-SalesData data=new SalesData();
+		SalesData data = new SalesData();
 
-displayGreeting();
-data.display();
+		displayGreeting();
+		data.display();
 
 	}
 
 	private static void displayGreeting() {
 		System.out.println("HELLO HAPPY SALES PEOPLE!");
 		System.out.println("THIS APP SHOWS SALES DATA");
-		
+
 		System.out.println("Test 1");
+		System.out.println("Test 2");
 	}
 }
