be588062fe881c52f20ca9dc3a8d97968ddc2b0c
root <root@localhost>
2016-04-25 13:34:47 +0200

    Add a simple output change.

--- SalesTool/src/dk/demo/salestool/Main.java
+++ SalesTool/src/dk/demo/salestool/Main.java
@@ -13,5 +13,7 @@ data.display();
 	private static void displayGreeting() {
 		System.out.println("HELLO HAPPY SALES PEOPLE!");
 		System.out.println("THIS APP SHOWS SALES DATA");
+		
+		System.out.println("Test 1");
 	}
 }
